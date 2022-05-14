c62b1be186691575fd7436f00c2e3e2562771221
Santiago Jimenez Giraldo <sago2k8@gmail.com>
2022-05-14 10:42:13 +0100

    Update plugin config, remove deprecation issues

--- after/plugin/lspkind.rc.lua
+++ after/plugin/lspkind.rc.lua
@@ -5,8 +5,6 @@ lspkind.init({
   -- enables text annotations
   --
   -- default: true
-  with_text = true,
-
   -- default symbol map
   -- can be either 'default' (requires nerd-fonts font) or
   -- 'codicons' for codicon preset (requires vscode-codicons font)
